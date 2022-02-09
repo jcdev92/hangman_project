32caa7ca04f85a8f34dcd090c73ef527588bdd32
jcdev <jcdeveloper92@gmail.com>
2022-02-09 10:13:09 -0400

    Try Rise Except Python

--- debugging.py
+++ debugging.py
@@ -1,15 +1,25 @@
 def divisors(num):
     divisors = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            divisors.append(i)
-    return divisors
+    try:
+        if num < 0:
+            raise ValueError("\nInserte solo numeros positivos")
+        for i in range(1, num + 1):
+            if num % i == 0:
+                divisors.append(i)
+        return divisors
+    except ValueError as ve:
+        print(ve)
+        return "vuelva a intentarlo"
 
 
 def run():
-    num = int(input('Ingresa un número: '))
-    print(divisors(num))
-    print("Terminó mi programa")
+    try:
+        num = int(input('Ingresa un número: '))
+        print(divisors(num))
+        print("\nTerminó mi programa")
+    except ValueError:
+        print("No se admiten letras o carcateres espciales, por favor, ingrese un numero")
+
 
 
 if __name__ == '__main__':
